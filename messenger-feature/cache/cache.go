210cb1cdb2b86b6e8af5d641c76ed5728a92ce99
Chris Vishoot <chrisvishoot@Chriss-MacBook-Pro.local>
2026-03-11 21:10:20 -0700

    Adding to the interface, wondering if I should setup Tilt or not

--- messenger-feature/cache/cache.go
+++ messenger-feature/cache/cache.go
@@ -1,6 +1,11 @@
 package cache
 
 type Cache interface {
-		
+	AddMessage(message string) error
 
+	GetMessages() ([]string, error)
+	GetMessageByID(id string) (string, error)
+	
+	DeleteMessage(message string) error
+	DeleteMessageByID(id string) error
 }
